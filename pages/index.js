dd2997aba91f2f3b556ff08cd87151420db2b2aa
goshieta <117492010+goshieta@users.noreply.github.com>
2023-03-13 14:26:07 +0900

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Load from '../components/top/load.jsx';
+import mainStyle from '../styles/top/style.css';
 
 export default function Home() {
   return (
