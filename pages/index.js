a2655d5bb19e1726a65b9f0e08f21b96344dfc0a
goshieta <117492010+goshieta@users.noreply.github.com>
2023-03-13 13:14:51 +0900

    finished basic setting.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import MainHead from '../components/mainHead.js';
 
 export default function Home() {
   return (
     <>
-      <MainHead></MainHead>
+      <Head>
+        <title>Inforde Top</title>
+      </Head>
     </>
   );
 }
