8e11a617bdda5024587d317bbd1b72cdb569169c
goshieta <117492010+goshieta@users.noreply.github.com>
2023-03-15 10:25:15 +0900

    画面切り替えの基本を作成

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Load from '../components/top/load.jsx';
+import { useState } from 'react';
 
 export default function Home() {
+  const [displayMode, setDisplayMode] = useState(0);
+  setTimeout(() => {
+    setDisplayMode(1);
+  }, 5000);
   return (
     <>
       <Head>
         <title>Inforde Top</title>
       </Head>
-      <Load />
+      <div style={{ display: displayMode == 0 ? 'block' : 'none' }}>
+        <Load />
+      </div>
+      <h1 style={{ display: displayMode == 1 ? 'block' : 'none' }}>
+        I'm a test man!
+      </h1>
     </>
   );
 }
