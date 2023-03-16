4c035647c80bdde8685a5e955a623a102f39fd57
goshieta <117492010+goshieta@users.noreply.github.com>
2023-03-16 09:59:27 +0900

    ページ、ウィジェットの基本を作成。

---
+++ components/top/wid.jsx
@@ -0,0 +1,5 @@
+import widBasic from '/widBasic.jsx';
+
+function clock() {
+  return;
+}
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,7 @@ import Load from '../components/top/load.jsx';
 import { useState } from 'react';
 
 export default function Home() {
-  const [displayMode, setDisplayMode] = useState(0);
-  setTimeout(() => {
-    setDisplayMode(1);
-  }, 5000);
+  const [displayMode, setDisplayMode] = useState(1);
   return (
     <>
       <Head>
@@ -16,9 +13,15 @@ export default function Home() {
       <div style={{ display: displayMode == 0 ? 'block' : 'none' }}>
         <Load />
       </div>
-      <h1 style={{ display: displayMode == 1 ? 'block' : 'none' }}>
-        I'm a test man!
-      </h1>
+      {/*コンテンツ自体 */}
+      <div id={styles.content}>
+        <div id={styles.top}></div>
+        <div id={styles.bottom}>
+          <div id={styles.left}></div>
+          <div id={styles.middle}></div>
+          <div id={styles.right}></div>
+        </div>
+      </div>
     </>
   );
 }
